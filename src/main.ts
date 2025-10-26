9030e1468c5da5818f78fe4b3188cd9c87d62c1b
Matheus Gabriel Pereira do Couto <matheus.gabriel.couto@hotmail.com>
2025-10-26 19:25:39 -0300

    a

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
 
   const configService: ConfigService<Env, true> = app.get(ConfigService)
   const port = configService.get('PORT', { infer: true })
-  await app.listen(port, '0.0.0.0')
+  await app.listen(port || 4000)
 }
 bootstrap()
